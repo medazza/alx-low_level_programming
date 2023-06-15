28571458f761992dc44573143cde16b7c4f0de27
medazza <midazza27@gmail.com>
2023-06-15 07:31:06 -0700

    create 5-printf.c  task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - print in screen : with proper grammar, but the ...
+ *Return: 0 -> success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
