718a5b69eb79b4d9b80211b05f233752ecb31d42
medazza <midazza27@gmail.com>
2023-06-16 05:09:27 -0700

    task 6 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry func
+ *Return: 0 >>>>>>>>success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
