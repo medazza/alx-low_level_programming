0d5fdf54dc843899d9b32ae35467ef1a720216e9
medazza <midazza27@gmail.com>
2023-06-16 04:31:32 -0700

    task 3 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry func
+ *Return: 0 ------------> success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
