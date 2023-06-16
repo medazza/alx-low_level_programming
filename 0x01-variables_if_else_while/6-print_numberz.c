a175e5e07ceffdd47e211b114978fa142a277f06
medazza <midazza27@gmail.com>
2023-06-16 05:17:15 -0700

    task 7 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry function
+ *return: 0 >>>>>>>>>>>> ok
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
