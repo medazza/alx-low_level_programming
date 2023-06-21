8f7d3eaa1e9102429f897fceb3c685a78495673f
medazza <midazza27@gmail.com>
2023-06-21 08:45:48 -0700

    task 1 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet -  prints the alphabet
+ * Descriptin:
+ * Return:
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
