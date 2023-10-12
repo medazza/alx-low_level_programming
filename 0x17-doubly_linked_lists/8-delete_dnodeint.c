546dbde90b50f1e6d2d7f3866579d944abbaf632
medazza <midazza27@gmail.com>
2023-10-12 11:07:08 -0700

    task 8 of 0x17-doubly_linked_lists update

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-/*
+/**
  * delete_dnodeint_at_index - deletes the node at
  *                             index of a dlistint_t linked list.
  * @head: Pointer to head of the dlistint_t.
